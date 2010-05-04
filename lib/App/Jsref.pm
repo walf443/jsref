dfb976ed42c815e04ce898a9b0128d0c0cb48b70
Keiji, Yoshimi <walf443@gmail.com>
2010-05-04 16:21:41 +0900

    initial commit

---
+++ lib/App/Jsref.pm
@@ -0,0 +1,33 @@
+package App::Jsref;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Jsref -
+
+=head1 SYNOPSIS
+
+  use App::Jsref;
+
+=head1 DESCRIPTION
+
+App::Jsref is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
